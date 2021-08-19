80a85aec50faa8006fb0f5fa8f1e928ca910514a
Kirill <rf>
2021-08-19 12:21:28 +0300

    ignore

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -75,7 +75,10 @@ WSGI_APPLICATION = 'geekshop.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
