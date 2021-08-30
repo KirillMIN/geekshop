2d061fa105d3d4cd34a1b2dc95e8c923841c75ad
Kirill <rf>
2021-08-30 15:47:56 +0300

    tasks(1-4)

---
+++ baskets/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from products.models import Product
+from baskets.models import Basket
+from django.shortcuts import HttpResponseRedirect
+
+# Create your views here.
+
+
+def basket_add(request, product_id):
+    product = Product.objects.get(id=product_id)
+    baskets = Basket.objects.filter(user=request.user, product=product)
+
+    if not baskets.exists():
+        Basket.objects.create(user=request.user, product=product, quantity=1)
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+    else:
+        basket = baskets.first()
+        basket.quantity += 1
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+
+
+
+
+
