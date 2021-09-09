b0f124e97d8b34d9d4f919f6d354d34bdd71990d
Kirill Minov <kirillminov@icloud.com>
2021-09-09 19:01:59 +0300

    '7 lesson'

--- admins/urls.py
+++ admins/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 
-from admins.views import index, admins_users, admin_users_create, create_user_update, admin_users_delete
+from admins.views import index, UserListView, UserCreateView, UserUpdateView, UserDeleteView
 
 app_name = 'admins'
 
 urlpatterns = [
     path('', index, name='index'),
-    path('users/', admins_users, name='admins_users'),
-    path('users_create/', admin_users_create, name='admin_users_create'),
-    path('users_update/<int:id>/', create_user_update, name='create_user_update'),
-    path('admin_users_delete/<int:id>/', admin_users_delete, name='admin_users_delete'),
+    path('users/', UserListView.as_view(), name='admins_users'),
+    path('users_create/', UserCreateView.as_view(), name='admin_users_create'),
+    path('users_update/<int:pk>/', UserUpdateView.as_view(), name='create_user_update'),
+    path('admin_users_delete/<int:pk>/', UserDeleteView.as_view(), name='admin_users_delete'),
 ]
 
